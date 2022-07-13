a247ed7668f985cc5d6672e44eb6c49c565e41bf
hassanshabbir279 <87772719+hassanshabbir279@users.noreply.github.com>
2022-07-13 12:01:11 -0700

    Add files via upload
    Setup Products On the Location

---
+++ src/components/Shopping.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+
+const Shopping = () => {
+  const [apiData, setApiData] = useState([]);
+
+  const getData = async () => {
+    let response = await fetch("https://fakestoreapi.com/products");
+    setApiData(await response.json());
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  let myStyle = {
+    width: "18rem",
+  };
+
+  return (
+    <div>
+      <hr />
+      <div class="d-flex justify-content-around main_cont">
+        <div className="container my-3">
+          <div class="row">
+            {apiData.map((curele) => {
+              return (
+                <>
+                  <div class="card my-3" style={myStyle}>
+                    <div class="row-md-12">
+                      <img src={curele.image} class="card-img-top" alt="..." />
+                      <div class="card-body">
+                        <h5 class="card-title">
+                          {curele.description.slice(0, 12)}
+                        </h5>
+                        <p class="card-text">
+                          <b>
+                            <i>{curele.category}</i>{" "}
+                          </b>
+                        </p>
+                        <b>Price : ${curele.price}</b>
+                        <br />
+                        <a href="#" class="btn btn-primary">
+                          Add to Cart
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Shopping;
